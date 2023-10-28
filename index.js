8b7165701766bf25a1a940c8ef6e7e1fcfef59a3
bella-t22 <b.jtorr22@gmail.com>
2023-10-27 18:11:06 -0700

    Add new book objects to library array

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ const myLibrary = [];
 const newBookBtn = document.getElementById('add-book-btn');
 const popUpForm = document.querySelector('.add-book-popup');
 const overlay = document.querySelector('.overlay');
+// const submitBtn = document.querySelector('.submit');
+const bookTitle = document.getElementById('book-title');
+const author = document.getElementById('author');
+const radioBtns = document.querySelectorAll('input[name="has-read"]');
+
+let checkedRadioBtn;
+function checkedRadio(radioBtns) {
+    for (radio of radioBtns) {
+        if (radio.checked) {
+            return checkedRadioBtn = radio;
+        }
+    }
+}
 
 newBookBtn.addEventListener('click', () => {
     popUpForm.classList.remove('hidden');
@@ -11,16 +24,23 @@ newBookBtn.addEventListener('click', () => {
 overlay.addEventListener('click', () => {
     popUpForm.classList.add('hidden');
     overlay.classList.add('hidden');
-}) 
+})
 
-// add object constructor for book creation
+popUpForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    checkedRadio(radioBtns);
+    const newBook = new Book(bookTitle.value, author.value, checkedRadioBtn.value);
+    addBookToLibrary(newBook);
+})
+
 function Book(title, author, hasRead) {
     this.title = title;
     this.author = author;
     this.hasRead = hasRead;
 }
 
-// write function to loop through and display each book in the array on the page
-function addBookToLibrary(title, author, hasRead) {
-    // const newBook = new Book()
+function addBookToLibrary(newBook) {
+    console.log(newBook);
+    myLibrary.push(newBook);
+    console.log(myLibrary);
 }
